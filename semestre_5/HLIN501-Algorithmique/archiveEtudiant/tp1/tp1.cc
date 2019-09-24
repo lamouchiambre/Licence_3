847f857e7b2ff609fb866587f67af36b61ab87db
ambre.lamouchi@etu.umontpellier.fr <ambre.lamouchi@etu.umontpellier.fr@i21.fdsetu.infra.umontpellier.fr>
2019-09-24 14:49:59 +0200

    fin du tp1 de graphe

--- semestre_5/HLIN501-Algorithmique/archiveEtudiant/tp1/tp1.cc
+++ semestre_5/HLIN501-Algorithmique/archiveEtudiant/tp1/tp1.cc
@@ -15,6 +15,7 @@ void grapheRandom(int n, int m, int edge[][2]){
 
 void composantes(int n, int m, int edge[][2], int comp[]){
   int x, y,aux;
+  
   for(int i = 0; i<n; i++){
     comp[i]=i;
   }
@@ -32,35 +33,72 @@ void composantes(int n, int m, int edge[][2], int comp[]){
   }
 }
 
+void composantesOpti(int n, int m, int edge[][2], int comp[]) {
+    int t[n] = {0}; // taille de comp[x]
+    stack<int> L[n]; // liste des sommets de comp[x], gérée par une pile
+    
+    for (int i = 0; i < n; i++) {
+        comp[i] = i;
+        L[comp[i]].push(i);
+        t[comp[i]] = 1;
+    }
+    for (int i = 0; i < m; i++) {
+        int x = edge[i][0];
+        int y = edge[i][1];
+        if (comp[x] != comp[y]) {
+            if (t[comp[x]] > t[comp[y]]) {
+                swap(x, y);
+            }
+            int aux = comp[x];
+            t[comp[y]] += t[aux];
+            while (!L[aux].empty()) {
+                comp[L[aux].top()] = comp[y];
+                L[comp[y]].push(L[aux].top());
+                L[aux].pop();
+            }
+        }
+    }
+}
+
+
+
+
 void ecritureTailles(int n, int m, int comp[]){
-  int nombreCompo[n];
-  int nbCop[m];
-  int tmp;
-  for(int i = 0; i<n; i++){
-    nombreCompo[i]=0;
+  int nbeComp[n]={0};
+  for(int i=0; i<n;i++){
+    nbeComp[comp[i]]++;
+    
   }
-  for(int i = 0; i<m; i++){
-    nbCop[i]=0;
+  cout<<endl<<endl;
+  int nbOcc[n]={0};
+  for(int j=0;j<n;j++){
+    //cout<<nbeComp[j]<<endl;
+      if (nbeComp[j]>0){
+	nbOcc[nbeComp[j]]++;
+      }
   }
-  for(int i = 0; i<n; i++){
-    tmp=comp[i];
-    nombreCompo[tmp]++;
-    cout <<"tmp="<<tmp<<" nombreCompo[tmp]:"<<nombreCompo[tmp]<<endl;
-  }
-  for(int i = 0; i<n; i++){
-    cout<<'['<<i<<','<<nombreCompo[i]<<']';
-  }
-  cout<<endl;
-  for(int i = 0; i<m; i++){
-    nbCop[nombreCompo[i]]++;  
-  }
-  for(int i = 0; i<m;i++){
-    //if(nombreCompo[i] != 0){
-    cout<< i+1<<" : "<<nbCop[i]<<endl;
-    // }
+  cout<<endl<<endl;
+  
+  for(int k=0;k<n;k++){
+    // cout<<nbOcc[k]<<endl;
+    if (nbOcc[k]>0){
+      cout<<"il y a "<< nbOcc[k] <<" composantes de taille "<< k<<endl;
+    }
   }
 }
 
+void afficheNbComp(int n, int tab[]) {
+  cout << endl;
+  for (int i = 1; i < n; i++) {
+    if (i == 0 && tab[i] != 0) {
+      cout << "Il y a " << tab[0] << " point(s) isole(s)." << endl;
+    }
+    if (tab[i] != 0) {
+      cout << "Il y a " << tab[i] << " composante(s) de taille " << i << "." << endl;     
+    }
+  }
+  cout << endl;
+}
 
 int main()
 { 
@@ -72,7 +110,7 @@ int main()
   cin >> m;
   int edge[m][2];    // Tableau des aretes.
   int comp[n];       // comp[i] est le numero de la composante contenant i.
-
+  
   grapheRandom(n,m,edge);
   cout << "Egde =" <<endl;
   for(int i = 0; i<m; i++){
@@ -86,7 +124,17 @@ int main()
     cout <<'['<<i<<"|"<<comp[i]<<"]";
   }
   cout <<endl;
-  ecritureTailles( n, m, comp);
+  composantesOpti(n,m, edge, comp);
+  cout << "Comp =" <<endl;
+  for(int i = 0; i<n; i++){
+    cout <<'['<<i<<"|"<<comp[i]<<"]";
+  }
+  cout <<endl;
+  //ecritureTailles( n, m, comp);
+  
+  
+  ecritureTailles(n, m, comp);
+  //afficheNbComp(n, nbComp);
   
   cout <<endl;
   
