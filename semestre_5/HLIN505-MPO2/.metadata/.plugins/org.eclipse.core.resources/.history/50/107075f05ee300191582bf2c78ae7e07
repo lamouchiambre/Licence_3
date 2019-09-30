6f69710488f8a42618168e2bc6d9b50ecc1806ee
ambre.lamouchi@etu.umontpellier.fr <ambre.lamouchi@etu.umontpellier.fr@t9.fdsetu.infra.umontpellier.fr>
2019-09-30 16:36:40 +0200

    tp3 java

---
+++ semestre_5/HLIN505-MPO2/.metadata/.plugins/org.eclipse.core.resources/.history/50/107075f05ee300191582bf2c78ae7e07
@@ -0,0 +1,5 @@
+package TP4;
+
+public interface Playlist extends ElementAudio {
+	public abstract int setnbElement();
+}
---
+++ semestre_5/HLIN505-MPO2/TP4/TP4/ElementAudio.java
@@ -0,0 +1,9 @@
+package TP4;
+
+public interface ElementAudio {
+	public abstract int getDuree();
+	public abstract String getTitre();
+	public abstract String getFichier();
+	
+	public abstract int getTaille(); 
+}
